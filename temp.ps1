e41f8a117606f38c8275a210b5d28b6339b69dbf
June Castillote <june.castillote@gmail.com>
2020-09-04 16:35:49 +0800

    Update temp.ps1

--- temp.ps1
+++ temp.ps1
@@ -1,8 +1,14 @@
 
+#poshlab.ga
 $ClientID = '94ba46ef-478c-4e8e-98e5-99a99a833db7'
 $Secret = 'ajC~M30veNTSm932.BzpXBb5ASb.G3V~t-'
-#$TenantID = 'b1f4ac95-b2d2-41db-ba4c-5627a94ad435'
-$TenantID = 'poshlab.ga'
+$TenantID = 'b1f4ac95-b2d2-41db-ba4c-5627a94ad435'
+
+#downergroup.com
+$ClientID = 'b70d3ea8-1691-4d57-9e8b-740f05bd4855'
+$Secret = 'jIUGFCdtga4iu/kBn7F06JbyhC8g7/0U05m/RC5IkA8='
+$TenantID = 'downergroup.com'
+
 $msalParams = @{
     ClientId = $ClientID
     TenantId = $TenantID
